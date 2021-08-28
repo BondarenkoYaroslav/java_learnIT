d84d983915aa0054fef1a244e78f169557559d2c
Anykeyman <Bondarenkorey@gmail.com>
2021-08-28 14:52:50 +0300

    Hello World Commit

---
+++ src/HelloWorldApp/HelloWorldDemo.java
@@ -0,0 +1,14 @@
+package HelloWorldApp;
+
+public class HelloWorldDemo {
+
+    public static void main(String[] args) {
+
+//        System.out.println("Hello World!");
+
+        PrintAction printAction = new PrintAction();
+
+        printAction.printMessage("Hello World!");
+
+    }
+}
---
+++ src/HelloWorldApp/PrintAction.java
@@ -0,0 +1,11 @@
+package HelloWorldApp;
+
+public class PrintAction {
+
+    public void printMessage(String message) {
+
+        System.out.println(message);
+
+    }
+
+}
